5a0845242497b14bda813d1617cad1e533b7fabb
UO264545 <UO264545@uniovi.es>
2020-04-13 15:26:17 +0200

    SDI–session10-Ejercicio-complementario2

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -152,32 +152,32 @@ module.exports = function(app, gestorBD) {
     });
 
     app.post("/api/autenticar/", function(req, res) {
-            let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
-                .update(req.body.password).digest('hex');
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
 
-            let criterio = {
-                email: req.body.email,
-                password: seguro
+        let criterio = {
+            email: req.body.email,
+            password: seguro
+        }
+
+        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+            if(usuarios == null || usuarios.length == 0) {
+                req.status(401);
+                res.json({
+                    autenticado: false
+                });
+            } else {
+                var token = app.get('jwt').sign(
+                    {usuario: criterio.email , tiempo: Date.now()/1000},
+                    "secreto");
+                res.status(200);
+                res.json({
+                    autenticado : true,
+                    token : token
+                });
             }
-
-            gestorBD.obtenerUsuarios(criterio, function(usuarios) {
-                if(usuarios == null || usuarios.length == 0) {
-                    req.status(401);
-                    res.json({
-                        autenticado: false
-                    });
-                } else {
-                    var token = app.get('jwt').sign(
-                        {usuario: criterio.email , tiempo: Date.now()/1000},
-                        "secreto");
-                    res.status(200);
-                    res.json({
-                        autenticado : true,
-                        token : token
-                    });
-                }
-            });
         });
+    });
 
     function check(validation, errorMessage, res) {
         if (!validation) {
